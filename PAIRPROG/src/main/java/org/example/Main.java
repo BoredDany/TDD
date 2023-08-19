a1ad0bcdbec52b774956433fbbd9d09318e1a05a
BoredDany <martinezq.d@javeriana.edu.co>
2023-08-18 22:43:23 -0500

    finished with inputs

--- PAIRPROG/src/main/java/org/example/Main.java
+++ PAIRPROG/src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Ejemplo de uso de la clase Salon y sus métodos
-        int[] arriveTimes = {-1, 0, 2, -3, 4};
-        int minStudents = 3;
+
+        int cant = 0, minStudents = 0;
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Write data: ");
+        cant = sn.nextInt();
+        minStudents = sn.nextInt();
+        int [] arriveTimes = new int [cant];
+        for(int i = 0 ; i < cant ; i++){
+            arriveTimes[i] = sn.nextInt();
+        }
+        System.out.println("cant: "+cant);
+        System.out.println("miin: "+minStudents);
+        System.out.println("array: ");
+        for(int i: arriveTimes){
+            System.out.print(i + " ");
+        }
 
         Salon salon = new Salon();
         salon.asign_min_students(minStudents,arriveTimes);
         salon.asign_array(arriveTimes);
 
-        System.out.println("Is class cancelled? " + salon.isClassCancelled());
+        System.out.println("\nIs class cancelled? " + salon.isClassCancelled());
 
-        // También puedes hacer otras pruebas y manipulaciones aquí
+        sn.close();
     }
 }
